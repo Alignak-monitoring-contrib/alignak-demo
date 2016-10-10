731d0d9405ee0019bd2f5b632b6624c32f930376
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-10-10 09:49:05 +0200

    Update  start/stop scripts - chown alignak

--- scripts/csh/alignak_start.sh
+++ scripts/csh/alignak_start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Set alignak user as owner (if anyone launched alignak as root...)
+chown alignak /usr/local/var/run/alignak/*
+chown alignak /usr/local/var/log/alignak/*
 su -m alignak << EOF
 /root/start_screens_alignak.sh &
 exit
