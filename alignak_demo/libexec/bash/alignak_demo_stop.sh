200bca49fdc4a2e37afa71eef6788ab699ee8b54
Frédéric MOHIER <frederic.mohier@gmail.com>
2017-01-18 20:22:01 +0100

    Update Alignak stop script for bash

--- alignak_demo/libexec/bash/alignak_demo_stop.sh
+++ alignak_demo/libexec/bash/alignak_demo_stop.sh
@@ -1,40 +1,18 @@
 #!/usr/bin/env bash
 echo "Stopping Alignak arbiter..."
-screen -X -S alignak_arbiter quit
+pkill -f alignak-arbiter
 echo "Stopped"
 
-echo "Stopping 'North' realm daemons..."
+echo "Stopping Alignak daemons..."
+echo " - broker..."
+pkill -f alignak-broker
+echo " - poller..."
+pkill -f alignak-poller
+echo " - scheduler..."
+pkill -f alignak-scheduler
 echo " - receiver..."
-screen -X -S alignak_north_receiver quit
-echo " - poller..."
-screen -X -S alignak_north_poller quit
-echo " - broker..."
-screen -X -S alignak_north_broker quit
-echo " - scheduler..."
-screen -X -S alignak_north_scheduler quit
-echo "Stopped"
-sleep 1
-
-echo "Stopping 'South' realm daemons..."
-echo " - poller..."
-screen -X -S alignak_south_poller quit
-echo " - broker..."
-screen -X -S alignak_south_broker quit
-echo " - scheduler..."
-screen -X -S alignak_south_scheduler quit
-echo "Stopped"
-sleep 1
-
-echo "Stopping 'All' realm daemons..."
-echo " - broker..."
-screen -X -S alignak_broker quit
-echo " - poller..."
-screen -X -S alignak_poller quit
-echo " - scheduler..."
-screen -X -S alignak_scheduler quit
-echo " - receiver..."
-screen -X -S alignak_receiver quit
+pkill -f alignak-receiver
 echo " - reactionner..."
-screen -X -S alignak_reactionner quit
+pkill -f alignak-reactionner
 echo "Stopped"
 sleep 1
