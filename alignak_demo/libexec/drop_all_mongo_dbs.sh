b80e6064872a469e4710f58fb80ec0aa7b4ab0fe
Frédéric MOHIER <frederic.mohier@gmail.com>
2017-03-08 14:18:21 +0100

    Removed the script - do not run correctly with MongoDB shell 3.4.2

--- alignak_demo/libexec/drop_all_mongo_dbs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-MyHOSTNAME="localhost";
-/etc/init.d/mongodb start
-
-# Drop all databases existing in the MongoDB server
-for i in $(mongo --quiet --host $MyHOSTNAME --eval "db.getMongo().getDBNames()" | tr "," " ");
-    do mongo $i --host $MyHOSTNAME --eval "db.dropDatabase()";
-done
